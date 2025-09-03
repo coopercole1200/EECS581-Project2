0f7baed135b083506c2edc215f1474adad592dde
ColinTreanor <colintreanor74@gmail.com>
2025-09-02 20:33:54 -0500

    add a start page and an option to select number of bombs

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 import pygame
+import pygame_widgets
+from pygame_widgets.button import Button 
+from pygame_widgets.dropdown import Dropdown
 
 pygame.init()
 
 # render variables 
-padding = 200
+padding = 50
 gameHeight = 10
 gameWidth = 10
 gameSize = 500
-padding = 50
 displaySize = gameSize + padding * 2
 cellSize = gameSize // gameWidth
 
 # display
-gameDisplay = pygame.display.set_mode((displaySize, displaySize)) #display
+gameDisplay = pygame.display.set_mode((displaySize, displaySize+100)) 
 pygame.display.set_caption("Minesweeper")  
 
-running = True
-while running:
-    grid =[]
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-   
-    # fill whole screen with gray 
-    gameDisplay.fill((200, 200, 200)) 
+# dropdown
+dropdown = Dropdown(
+    gameDisplay, (displaySize//2)+100, (displaySize//2)-200, 60, 50,
+    name='Bombs',
+    choices=['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
+    borderRadius=3,
+    colour=(255,255,255),
+    direction='down',
+    textHAlign='left'
+)
 
+MENU = True
+GAME = False
+
+# button
+startButton = pygame.Rect((displaySize//2)-125, 100, 200, 60)
+
+def drawStartMenu(): 
+    title = pygame.font.Font(None, 60).render("Minesweeper", True, (0,0,0))
+    startText = pygame.font.Font(None, 30).render("Start", True, (0,0,0))
+    
+    gameDisplay.blit(title,(displaySize//2 - title.get_width()//2, 25))
+    
+    mouse_pos = pygame.mouse.get_pos()
+    if startButton.collidepoint(mouse_pos):
+        pygame.draw.rect(gameDisplay, (0, 150, 255), startButton)
+    else:
+        pygame.draw.rect(gameDisplay, (0, 100, 255), startButton)
+
+    gameDisplay.blit(startText, (startButton.centerx - startText.get_width()//2, startButton.centery - startText.get_height()//2))
+
+def drawGame():
     # draw white background
     pygame.draw.rect(gameDisplay, (255, 255, 255),
                      (padding, padding, gameSize, gameSize))
@@ -35,6 +59,37 @@ while running:
     for y in range(0, gameSize + 1, cellSize):
         pygame.draw.line(gameDisplay, (0, 0, 0), (padding, padding + y), (padding + gameSize, padding + y))
 
-    pygame.display.flip()
+running = True
+
+while running:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+        if MENU:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if startButton.collidepoint(event.pos):
+                    # store number of bombs user selects 
+                    numBombs = dropdown.getSelected()
+                    dropdown = None
+                    MENU = False
+                    GAME = True
+        elif GAME:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+    
+    # fill screen with gray
+    gameDisplay.fill((200, 200, 200))
+
+    # draw menu or game
+    if MENU: 
+        drawStartMenu()
+        # update widgets
+        pygame_widgets.update(events)
+    elif GAME: 
+        drawGame()
+
+    # update screen
+    pygame.display.update()
 
 pygame.quit()
