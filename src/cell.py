3150bf25d16b8cb54201793d495b07281168f886
Ryland Edwards <rylandcedwards@gmail.com>
2025-09-14 13:46:26 -0500

    added windows vs mac, needs to be tested on windows

--- src/cell.py
+++ src/cell.py
@@ -5,6 +5,7 @@ Class definition for individual cell of minesweeper game'''
 
 import pygame
 from math import sin
+import platform
 
 class Cell(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -18,25 +19,45 @@ class Cell(pygame.sprite.Sprite):
 
         #pygame init
         super().__init__()  # Initialize the parent Sprite class
-        self.image = pygame.image.load('textures\\Unrevealed_Tile.png').convert_alpha()  # Load the sprite image
+
+        if platform.system() == 'Windows':
+            self.image = pygame.image.load('textures\\Unrevealed_Tile.png').convert_alpha()  # Load the sprite image
+        else:
+            self.image = pygame.image.load('textures/Unrevealed_Tile.png').convert_alpha()  # Load the sprite image
+
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()  # Get the rectangle for collision and positioning
         self.rect.topleft = (x, y)  # Set the initial position
     
     def update(self):
         #funct that updates sprite image based on state
-        if not self.revealed:
-            if self.flagged:
-                self.image = pygame.image.load('textures\\Flagged_Tile.png').convert_alpha()
+
+        if platform.system() == 'Windows':
+            if not self.revealed:
+                if self.flagged:
+                    self.image = pygame.image.load('textures\\Flagged_Tile.png').convert_alpha()
+                else:
+                    self.image = pygame.image.load('textures\\Unrevealed_Tile.png').convert_alpha()
             else:
-                self.image = pygame.image.load('textures\\Unrevealed_Tile.png').convert_alpha()
+                if self.bomb:
+                    self.image = pygame.image.load('textures\\Mine_Tile.png').convert_alpha()
+                elif self.nearby != 0:
+                    self.image = pygame.image.load(f'textures\\Tile_{self.nearby}.png').convert_alpha()
+                else:
+                    self.image = pygame.image.load('textures\\Revealed_Tile.png').convert_alpha()
         else:
-            if self.bomb:
-                self.image = pygame.image.load('textures\\Mine_Tile.png').convert_alpha()
-            elif self.nearby != 0:
-                self.image = pygame.image.load(f'textures\\Tile_{self.nearby}.png').convert_alpha()
+            if not self.revealed:
+                if self.flagged:
+                    self.image = pygame.image.load('textures/Flagged_Tile.png').convert_alpha()
+                else:
+                    self.image = pygame.image.load('textures/Unrevealed_Tile.png').convert_alpha()
             else:
-                self.image = pygame.image.load('textures\\Revealed_Tile.png').convert_alpha()
+                if self.bomb:
+                    self.image = pygame.image.load('textures/Mine_Tile.png').convert_alpha()
+                elif self.nearby != 0:
+                    self.image = pygame.image.load(f'textures/Tile_{self.nearby}.png').convert_alpha()
+                else:
+                    self.image = pygame.image.load('textures/Revealed_Tile.png').convert_alpha()
 
         self.image = pygame.transform.scale(self.image, (50, 50))
 
